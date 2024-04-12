354b27c82aba820c320b3240ba627503d6666ebe
ajith317 <ajithbabu517@gmail.com>
2024-04-12 11:40:57 +0530

    QuizGame integrated

---
+++ Invite/js/gameQuestions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Where Ajith meet anu?",
+    answer: "In Collage",
+    options: [
+      "In Collage",
+      "Area",
+      "Other question",
+      "Dont know"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What food will provide on devgas?",
+    answer: "No giving anything",
+    options: [
+      "Parotta",
+      "Chapptathi",
+      "Rice",
+      "No giving anything"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What food will provide on devgas?",
+    answer: "No giving anything",
+    options: [
+      "Parotta",
+      "Chapptathi",
+      "Rice",
+      "No giving anything"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What food will provide on devgas?",
+    answer: "No giving anything",
+    options: [
+      "Parotta",
+      "Chapptathi",
+      "Rice",
+      "No giving anything"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What food will provide on devgas?",
+    answer: "No giving anything",
+    options: [
+      "Parotta",
+      "Chapptathi",
+      "Rice",
+      "No giving anything"
+    ]
+  },
+    {
+      numb: 6,
+      question: "What food will provide on devgas?",
+      answer: "No giving anything",
+      options: [
+        "Parotta",
+        "Chapptathi",
+        "Rice",
+        "No giving anything"
+      ]
+    },
+      {
+      numb: 7,
+      question: "What food will provide on devgas?",
+      answer: "No giving anything",
+      options: [
+        "Parotta",
+        "Chapptathi",
+        "Rice",
+        "No giving anything"
+      ]
+    },
+      {
+      numb: 8,
+      question: "What food will provide on devgas?",
+      answer: "No giving anything",
+      options: [
+        "Parotta",
+        "Chapptathi",
+        "Rice",
+        "No giving anything"
+      ]
+    },
+      {
+      numb: 9,
+      question: "What food will provide on devgas?",
+      answer: "No giving anything",
+      options: [
+        "Parotta",
+        "Chapptathi",
+        "Rice",
+        "No giving anything"
+      ]
+    },
+      {
+      numb: 10,
+      question: "What food will provide on devgas?",
+      answer: "No giving anything",
+      options: [
+        "Parotta",
+        "Chapptathi",
+        "Rice",
+        "No giving anything"
+    ]
+    },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
